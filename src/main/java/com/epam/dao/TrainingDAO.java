bccb45ba6d9f64d0f5138b89337e48325ff3a0e3
Felipe Munevar N <felipemunevarn@gmail.com>
2025-03-27 20:13:38 -0300

    add trainingDAO

---
+++ src/main/java/com/epam/dao/TrainingDAO.java
@@ -0,0 +1,35 @@
+package com.epam.dao;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import com.epam.model.Training;
+
+@Repository
+public class TrainingDAO {
+    private final Map<String, Training> storage;
+
+    @Autowired
+    public TrainingDAO(@Qualifier("traineeStorage") Map<String, Training> storage) {
+        this.storage = storage;
+    }
+
+    public void save(Training training) {
+        storage.put(training.getId(), training);
+    }
+
+    public Training findById(String id) {
+        return storage.get(id);
+    }
+
+    public boolean exists(String id) {
+        return storage.containsKey(id);
+    }
+ 
+    public Map<String, Training> getStorage() {
+        return storage;
+    }
+}
